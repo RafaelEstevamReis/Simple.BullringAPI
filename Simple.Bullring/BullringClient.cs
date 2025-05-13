d78fbd625048e1aeb3fa334e66213dfda4e10f4f
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-05-13 12:42:22 -0300

    Create BullringClient.cs

---
+++ Simple.Bullring/BullringClient.cs
@@ -0,0 +1,21 @@
+﻿namespace Simple.Bullring;
+
+using Simple.Bullring.Models;
+using System;
+using System.Threading.Tasks;
+
+public class BullringClient
+{
+    private readonly API.ClientInfo client;
+    public API.ClientInfo InternalClient => client;
+
+    private BullringClient(string apiKey, string url)
+    {
+        client = new API.ClientInfo(url);
+        client.SetHeader("x-api-key", apiKey);
+    }
+
+    public BullringClient(string apiKey) : this(apiKey, "https://api.bullring.finance/") { }
+    public static BullringClient FromStagingEnviroment(string apiKey) => new BullringClient(apiKey, "https://staging-api.bullring.finance/");
+
+}
